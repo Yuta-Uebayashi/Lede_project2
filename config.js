25476603f6a91e9013324a2a0cd1fcfba7e63b7f
Yuta-Uebayashi <yuta.uebayashi@nex.nikkei.com>
2025-07-11 19:00:40 -0400

    change name

--- config.js
+++ config.js
@@ -62,7 +62,7 @@ var config = {
             alignment: 'center',
             hidden: false,
             title: 'The Number of Foreign Residents in Japan Is Growing Rapidly', 
-            image: './assets/The Number of Foreigners Is Increasing Rapidly.png',
+            image: './Graphs_and_Maps/The Number of Foreigners Is Increasing Rapidly.png',
             description: '',
             location: {
                 center: [139.00666, 38.04727],
@@ -83,7 +83,7 @@ var config = {
             alignment: 'center',
             hidden: false,
             title: 'Share of Foreign Residents by Prefecture',
-            image: './assets/foreign-resident-ratio-by-prefecture.png',
+            image: './Graphs_and_Maps/foreign-resident-ratio-by-prefecture.png',
             description: 'Using data from the 2020 census, I analyzed the percentage of foreign residents in each prefecture. <br> In major metropolitan areas like Tokyo, Osaka, and Aichi, the percentage of foreign residents exceeded 3%.In contrast, many rural municipalities in regions such as Tohoku and Kyushu had less than 1%.',
             location: {
                 center: [139.00666, 38.04727],
@@ -166,7 +166,7 @@ var config = {
             alignment: 'center',
             hidden: false,
             title: 'Foreign Resident Ratio by Municipality in Hokkaido',
-            image: './assets/Data by Municipality in Hokkaido.png',
+            image: './Graphs_and_Maps/Data by Municipality in Hokkaido.png',
             description: 'Regional patterns vary across Japan. <br> Let’s begin by examining Hokkaido, where certain municipalities now have some of the highest foreign resident ratios in the country.',
             location: {
                 center: [143.5, 43.29072],
@@ -185,7 +185,7 @@ var config = {
             alignment: 'right',
             hidden: false,
             title: '',
-            image: './assets/Shimukappu_image2.jpg',
+            image: './Graphs_and_Maps/Shimukappu_image2.jpg',
             description: 'In Shimukappu Village, the foreign resident ratio was just 1% in 2010, but it jumped to 11% by 2020. Since then, the number has continued to rise, and the most recent data shows that one in three residents is now a foreign national. Most of the newcomers have come from Asian countries, particularly China.',
             location: {
                 center: [142.39691, 42.98284],
@@ -204,7 +204,7 @@ var config = {
             alignment: 'right',
             hidden: false,
             title: '',
-            image: './assets/Shimukappu_image.png',
+            image: './Graphs_and_Maps/Shimukappu_image.png',
             description: 'Shimukappu Village is known for its ski resorts and attracts many foreign tourists every winter.As a result, demand for foreign workers has been growing, especially at local ski areas and lodging facilities.',
             location: {
                 center: [142.39691, 42.98284],
@@ -223,7 +223,7 @@ var config = {
             alignment: 'center',
             hidden: false,
             title: 'Foreign Resident Ratio by Municipality in Kanto',
-            image: './assets/Data by Municipality in Kanto.png',
+            image: './Graphs_and_Maps/Data by Municipality in Kanto.png',
             description: 'The Kanto region, which includes Tokyo and surrounding prefectures, has some of the highest concentrations of foreign residents in Japan. Several municipalities, especially those in central Tokyo and parts of Saitama and Kanagawa, have foreign resident ratios above 5%, with a few exceeding 10%.',
             location: {
                 center: [139.75394, 35.79465],
@@ -242,7 +242,7 @@ var config = {
             alignment: 'right',
             hidden: false,
             title: '',
-            image: './assets/Kawakami_image.jpg',
+            image: './Graphs_and_Maps/Kawakami_image.jpg',
             description: 'Kawakami Village in Nagano Prefecture stands out with the highest percentage of foreign residents in the Kanto region—nearly 19% as of 2020. This small mountain village, with a population of around 4,000, is known for its highland lettuce farming, which requires a large seasonal workforce.',
             location: {
                 center: [138.65040, 35.96299],
@@ -261,7 +261,7 @@ var config = {
             alignment: 'right',
             hidden: false,
             title: '',
-            image: './assets/Kawakami_image2.jpg',
+            image: './Graphs_and_Maps/Kawakami_image2.jpg',
             description: 'In recent years, many of these agricultural workers have come from countries such as Vietnam and China under Japan’s technical trainee program. Because of this trend, foreign residents now make up almost one in five people in the village. <br> Despite its rural setting, Kawakami has become an example of how even remote areas in Japan are becoming increasingly dependent on foreign labor.',
             location: {
                 center: [138.65040, 35.96299],
@@ -280,7 +280,7 @@ var config = {
             alignment: 'left',
             hidden: false,
             title: 'Onna Village, Okinawa',
-            image: './assets/Onnnason_image.jpg',
+            image: './Graphs_and_Maps/Onnnason_image.jpg',
             description: 'Onna Village in Okinawa Prefecture had a foreign resident ratio of 6.8% as of 2020—one of the highest in the southern islands of Japan. Located along the island’s scenic western coastline, Onna is a popular resort area known for its beaches, diving spots, and luxury hotels.',
             location: {
                 center: [127.85367, 26.49741],
@@ -299,7 +299,7 @@ var config = {
             alignment: 'left',
             hidden: false,
             title: '',
-            image: './assets/Onnason_image2.jpg',
+            image: './Graphs_and_Maps/Onnason_image2.jpg',
             description: 'In addition to tourism, the presence of nearby U.S. military bases also plays a significant role in shaping Onna Village’s foreign resident population. Onna hosts 7.9% of all U.S. military base area in Okinawa Prefecture, making it one of the municipalities most affected by the American military presence.Many U.S. servicemembers and their families live in the area.',
             location: {
                 center: [127.85367, 26.49741],
@@ -318,7 +318,7 @@ var config = {
             alignment: 'center',
             hidden: false,
             title: 'Do More Foreign Residents Mean Higher Local Tax Revenue?',
-            image: './assets/Does an increase in foreign residents lead to higher municipal tax revenue_.png',
+            image: './Graphs_and_Maps/Does an increase in foreign residents lead to higher municipal tax revenue_.png',
             description: 'This chart is based on municipal-level tax data provided by Japan’s Ministry of Internal Affairs and Communications. It focuses on municipalities where the foreign resident population increased by more than 3% between 2010 and 2020. <br> The analysis aimed to see whether growth in foreign residents leads to higher local tax revenue—a sign of stronger economic activity. <br> <br> However, the results suggest that the connection may be weak. Among the municipalities analyzed, only four exceeded the national average for tax revenue growth, despite significant growth in their foreign populations.',
             location: {
                 center: [139.00666, 38.04727],
